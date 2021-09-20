a70a124b7310906589503cb1c1716859af225021
rajeev <rajeev.bhardwaj.45@gmail.com>
2021-09-20 16:33:44 +0530

    Jenkins url

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,5 @@
 docker stop flask-container
 docker rm flask-container
 docker run -di --name flask-container -p 5000:5000 flask-image-new /bin/bash
+docker exec -it flask-container /bin/bash/
+python app.py --host=0.0.0.0
